30197a16d95f74964fffc67c91cc872176f535fb
Paul Zeiger <paul.zeiger@uni-konstanz.de>
2016-09-06 19:50:37 +0200

    v1.1 - gnuplot_script.sh

--- gnuplot_script.sh
+++ gnuplot_script.sh
@@ -14,9 +14,14 @@ cp $file ${gnuin}_tmp
 grep 'set output' $file | awk '{ print $NF }' | cut -d ''"'"'' -f 2 | xargs -n 1 -I {} sed -i 's:'{}':'$dir/{}':' ${gnuin}_tmp
 
 # modify data path
-grep 'file = ' $file | awk '{ print $NF }' | cut -d '"' -f 2 | xargs -n 1 -I {} sed -i 's:'{}':'$dir/{}':' ${gnuin}_tmp
+PATT=$(grep 'file = ' $file | awk '{ print $NF }' | cut -d '"' -f 2)
+echo $PATT
+grep 'file = ' $file | awk '{ print $NF }' | cut -d '"' -f 2 | awk 'BEGIN { FS = "*" }; {if ($2 == "") print $1; else print $1"[*]"$2}' | xargs -n 1 -I {} sed -i 's:'{}':'$dir/$PATT':' ${gnuin}_tmp
+#echo $( grep 'file = ' $file | awk '{ print $NF }' | cut -d '"' -f 2 | awk 'BEGIN { FS = "*" }; {if ($2 == "") print $1; else print $1"[*]"$2}')
 
 gnuplot ${gnuin}_tmp
 
-rm -r ${gnuin}_tmp
+mkdir -p gnuplot_tmp
+
+mv ${gnuin}_tmp gnuplot_tmp/
 
